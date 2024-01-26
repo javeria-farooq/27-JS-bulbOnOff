7481cf8771bb8c07a3437c2e54128b5f71057fd3
javeria-farooq <gettingeducation6@gmail.com>
2024-01-26 15:57:20 +0500

    27 JS Assignment

---
+++ app.js
@@ -0,0 +1,45 @@
+// function bulbon() {
+//     document.getElementById('img').src = 'https://thumbs.dreamstime.com/b/light-bulb-black-background-17047477.jpg'
+// }
+
+// function bulboff() {
+//     document.getElementById('img').src = 'https://media.istockphoto.com/id/502789173/photo/light-bulb-concept.jpg?s=612x612&w=0&k=20&c=vJfSDuFJf_4UsQcUsdMNUfDkkNU6Bf9cTHF-n4-Zwvs='
+// }
+
+// task :
+
+// let on = document.getElementById('img').src = 'https://thumbs.dreamstime.com/b/light-bulb-black-background-17047477.jpg'
+// let a = on
+
+// let off = document.getElementById('img').src = 'https:media.istockphoto.com/id/502789173/photo/light-bulb-concept.jpg?s=612x612&w=0&k=20&c=vJfSDuFJf_4UsQcUsdMNUfDkkNU6Bf9cTHF-n4-Zwvs='
+// let b = off
+
+// let btn = document.getElementById('btn')
+
+
+// function bulbonOff () {
+//     if (btn === on) {
+//         btn === off
+//     }
+
+//     if (btn === off) {
+//         btn === on
+//     }
+// }
+
+
+// I am fail in this task.
+
+//                  ternary operator:
+
+// let a = 5
+// let a = 6
+// let b = (a === 5 ? 'hn thk h' : 'glt h' )
+// ab hmne if ka kaam kia h ? mark se phr kaha k agr cndition true h to aage chlo or thk h
+// print krwao phr colon lga k else ka kaam strt kia k check kro agr cndtion false h t 
+// aage chlo or glt h print krwao
+// document.write(b)
+
+// let myTernary = 8
+// let myFinalTernary = (myTernary === 5 ? 'done h' : 'try again')
+// document.write(myFinalTernary)
